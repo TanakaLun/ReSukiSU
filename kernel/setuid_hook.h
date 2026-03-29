tiann/KernelSU

kernel: use syscall table hook to avoid running in atomic context
- Replace kprobes in ksud.c with raw syscall table hook
- Handle sucompat and setuid hook with unused syscalls, which is redirected by tracepoint
- Make most of atomic-context-unsafe code running in normal context
- Solve #3287

Co-authored-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>

kernel/setuid_hook.h
@@ -8,6 +8,6 @@ void ksu_setuid_hook_init(void);
 void ksu_setuid_hook_exit(void);
 
 // Handler functions for hook_manager
-int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
+int ksu_handle_setresuid(uid_t old_uid, uid_t new_uid);
 
 #endif

TanakaLun/ReSukiSU

- Solve https://github.com/tiann/KernelSU/issues/3287
AlexLiuDev233:
Resolve manual hook/susfs inline hook compatible
And clean up code logic by remove task_work in tp hook too
[cherry-picked from upstream commit https://github.com/tiann/KernelSU/commit/225ffbbf6bd48e22c6b232a31e705a538664eade]
Co-authored-by: AlexLiuDev233 <wzylin11@outlook.com>
Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>

kernel/setuid_hook.h
@@ -7,6 +7,6 @@
 void ksu_setuid_hook_init(void);
 void ksu_setuid_hook_exit(void);
 
-int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
+int ksu_handle_setuid(uid_t new_uid, uid_t old_uid);
 
 #endif